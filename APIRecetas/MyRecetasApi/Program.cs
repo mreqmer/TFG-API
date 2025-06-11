e5a4fb0576d79cd7ace3a1fe448c5d4241cce5eb
mreqmer <145675425+mreqmer@users.noreply.github.com>
2025-06-11 23:02:54 +0200

    cambio release

--- APIRecetas/MyRecetasApi/Program.cs
+++ APIRecetas/MyRecetasApi/Program.cs
@@ -11,8 +11,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
